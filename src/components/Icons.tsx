a4e926c27c1272abc5a636f1589fa2a9c5da0bec
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 17:33:53 +0100

    Add fontawesome and test using home icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Icons = () => {
   return (
-    <div>Icons</div>
-  )
-}
+    <div>
+      Icons
+      <i className="fas fa-home"></i> Home
+    </div>
+  );
+};
 
-export default Icons
+export default Icons;
