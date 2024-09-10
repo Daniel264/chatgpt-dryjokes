6ba7ac23036bafea62b7642b7c4f5050c4c07489
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 15:15:08 +0100

    display the first joke only

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import "./App.scss";
 import Button from "./components/Button";
+import Jokes from "./interface/jokes";
+import { jokeDictionary } from "./interface/jokes";
 
 function App() {
+  const joke = jokeDictionary[0];
   return (
     <>
       <div className="main-box">
         <div className="joke-box">
-          <p>Joke #1</p>
+          <p>Joke # {joke.id}</p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quis
-            itaque deleniti sint perspiciatis vitae doloremque inventore ad
-            magni ab!
+            {joke.text}
           </p>
           <Button />
         </div>
