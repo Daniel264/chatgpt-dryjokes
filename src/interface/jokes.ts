298401f7bb0bb16e99440635f8c4d289fec9ce53
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 18:00:09 +0100

    Add more jokes

--- src/interface/jokes.ts
+++ src/interface/jokes.ts
@@ -5,6 +5,10 @@ interface Jokes {
 
 export const jokeDictionary = [
   {
+    id: 0,
+    text: "Why did the student eat his homework? Because his teacher told him it was a piece of cake!",
+  },
+  {
     id: 1,
     text: "Why don’t skeletons fight each other? Because they don’t have the guts.",
   },
@@ -12,6 +16,24 @@ export const jokeDictionary = [
   {
     id: 3,
     text: "Why don’t scientists trust atoms? Because they make up everything!",
+  },
+  {
+    id: 4,
+    text: "Why did the math teacher break up with the calculator? She felt like he was just adding to her problems.",
+  },
+  {
+    id: 5,
+    text: "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  },
+  { id: 6, text: "Why did the bicycle fall over? Because it was two-tired!" },
+  {
+    id: 7,
+    text: "Why don’t some couples go to the gym? Because some relationships don’t work out.",
+  },
+  { id: 8, text: "Why don’t programmers like nature? It has too many bugs." },
+  {
+    id: 9,
+    text: "Why did the tomato turn red? Because it saw the salad dressing!",
   },
 ];
 
