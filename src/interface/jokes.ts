fce1982572fb1ffc1de0e958acc651afd30af058
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 15:09:34 +0100

    add few jokes to an object

--- src/interface/jokes.ts
+++ src/interface/jokes.ts
@@ -3,4 +3,16 @@ interface Jokes {
   text: string;
 }
 
+export const jokeDictionary = [
+  {
+    id: 1,
+    text: "Why don’t skeletons fight each other? Because they don’t have the guts.",
+  },
+  { id: 2, text: "Why was the math book sad? It had too many problems." },
+  {
+    id: 3,
+    text: "Why don’t scientists trust atoms? Because they make up everything!",
+  },
+];
+
 export default Jokes;
