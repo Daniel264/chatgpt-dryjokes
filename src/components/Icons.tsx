0d367aa747cbe0c32cc1f64c9d2376140f3d03f2
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 18:08:55 +0100

    add bounce animation

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -3,7 +3,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const Icons = () => {
   return (
     <div>
-      <i className="fab fa-whatsapp"></i>
+      <i className="fab fa-whatsapp bounce"></i>
       <i className="fab fa-facebook"></i>
       <i className="fab fa-linkedin"></i>
     </div>
