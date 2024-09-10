c3b40646b56d01e38d261854ff762b3a42b597eb
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 13:40:18 +0100

    Add box-shadow to mixin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,16 @@ import Button from "./components/button";
 function App() {
   return (
     <>
+      <div className="main-box">
+        <div className="joke-box">
+          <p>Joke #1</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At quis
+            itaque deleniti sint perspiciatis vitae doloremque inventore ad
+            magni ab!
+          </p>
+        </div>
+      </div>
       <Button />
     </>
   );
