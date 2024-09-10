321488411e0d69f90a9265c3d59a8917b0d9d57d
Daniel Olatinsu <danieloluwafolajimi@gmail.com>
2024-09-10 16:11:45 +0100

    Add icons component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.scss";
 import Button from "./components/Button";
 import { jokeDictionary } from "./interface/jokes";
+import Icons from "./components/Icons";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -23,6 +24,7 @@ function App() {
             <p className="header">Joke # {joke.id}</p>
             <p>{joke.text}</p>
           </div>
+          <Icons />
           <div className="button-box">
             <Button onClick={handleClick} />
           </div>
